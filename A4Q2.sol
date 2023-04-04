d87898218f0a0df3edfd20cfbeabc626c3a84953
Srihari651 <126260054+Srihari651@users.noreply.github.com>
2023-04-04 04:00:15 -0700

    Add files via upload

---
+++ A4Q2.sol
@@ -0,0 +1,48 @@
+pragma solidity ^0.8.1;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+contract crowdFunding
+{
+    using SafeMath for uint256;
+    enum State {inactive,active,completed} //declares three states of the contract
+    State public state;
+    uint256 public fundingGoal; //total funding goal is declared as an integer
+    uint256 public deadLine; //deadline of the contract declared as an integer
+    constructor() public
+    {
+        state==State.inactive; //shows that the contract is in inactive state
+    }
+    address payable public fundAddress = payable (address(0x617F2E2fD72FD9D5503197092aC168c91465E7f2)); //represents the address to which the fundis raised
+
+    function sendFunding(uint256 goalAmount, uint256 durationDays) external payable returns (bool)
+    {
+        fundingGoal=goalAmount; //represents the total amount of the goal
+        uint256 a = 6000;
+        deadLine = block.timestamp.add(a.mul(60).mul(24).mul(durationDays)); //gives the deadline of the contract in days
+        state=State.active; //shows boolean value 1 when the contract is in active state
+        return state==State.active;
+    }
+
+    function transfer(uint256 amount) external payable
+    {
+        fundAddress.transfer(amount); //transfers the fund amount from sender to the mentioned fund address
+    }
+
+    function fundBalance() public view returns(uint256 a)
+    {
+        a=(address(0x617F2E2fD72FD9D5503197092aC168c91465E7f2).balance); //gives the total balance of the fund
+        return a;
+    }
+    
+    function endFunding() public view returns(string memory)
+    {
+        uint256 a = address(0x617F2E2fD72FD9D5503197092aC168c91465E7f2).balance ;
+        if (a >= fundingGoal) //condition statement to returns if the funds collected is greater than the goal
+        { 
+            return "completed and successful"; //returns the message if the contract is successful
+        }
+        else
+        {
+            return "completed but unsuccessful"; //returns the message if the contract is unsuccesssful
+        }
+    }
+}
---
+++ A4Q3.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.1;
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+contract contractSubscription
+{
+    using SafeMath for uint256;
+    enum State {inactive} //declares the the states of the contract
+    State public state;
+    bool private active; //boolean type to represents the active state of the subscription
+
+    constructor() public 
+    {
+        state=State.inactive; //returns 0 if he contract is in inactive state
+    }
+
+    function activateSubscription() public 
+    {
+        active = true; //show that the contract is in active state
+    }
+
+    address public contactAddress = payable (address(0x617F2E2fD72FD9D5503197092aC168c91465E7f2)); //address that sender will pay for the subscription
+    
+    function subscribe() external payable onlyWhileActive() 
+    {
+        payable(contactAddress).transfer(10 ether); //statement that transfers the mentioned amount of ethers
+    }
+
+    modifier onlyWhileActive() //modifer that will execute before the above mentioned function which checks thats the system is active
+    {
+        require(active==true); //gives error if the condition is false
+        _;
+    }
+
+    function timePeriod() public view  returns(string memory payFee, uint durationDays) 
+    {
+        uint256 a = 6000;
+        uint256 b = 10;
+        return (payFee = "10 ethers", durationDays = (((block.timestamp.add(b.mul(a.mul(60).mul(24)))).div(a.mul(60).mul(24)))).sub(194)); //return statement that shows the subcription fees and the duration of the service to the user
+    }
+}
